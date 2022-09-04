5b9fe10fde8a4136c6a74808d977e04533f86042
Erfanprg1701 <erfan.prg1701@GMAIL.com>
2022-09-04 11:56:05 +0430

    add navbar

---
+++ src/components/global/NavBar/MegaMenuCategory.vue
@@ -0,0 +1,47 @@
+<script setup>
+import dataCategory from '@/data/dataCategory';
+import { ref } from 'vue';
+import ContentMegaMenu from './ContentMegaMenu.vue';
+
+const dataCat = ref(dataCategory)
+const idCat = ref(1)
+
+const selectCategory = (id) => {
+    idCat.value = id
+}
+
+</script>
+
+<template>
+    <div class="w-full flex py-2 px-1 overflow-hidden shadow-md rounded-md">
+        <div class="flex flex-col w-[200px] text-[12px] border-l border-neutral200 text-neutral700">
+            <div v-for="data in dataCat">
+                <a class="w-full flex items-center py-[10px] px-2 box-border hover:bg-[#f7f7f7] hover:text-primary500" href="#" @mouseover="selectCategory(data.id)">
+                    <i class="ml-2 text-lg" :class="data.iconTitle"></i>
+                    <p>{{ data.title }}</p>
+                </a>
+            </div>
+        </div>
+        <div class="flex flex-col pt-3 pb-5 px-6 w-[80%]">
+            <div class="w-full flex items-cente mb-6 text-neutral600 text-[13px] font-semibold">
+                <a href="#">همه محصولات {{ dataCat[idCat].title }} </a>
+                <i class="uil uil-angle-left-b mr-2"></i>
+            </div>
+            <div class="text-neutral600">
+                <ContentMegaMenu :data-content="dataCat[idCat].content"/>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<style> 
+
+.test {
+    color: #fafafa;
+}
+</style>
+
+
+
+
---
+++ src/components/global/NavBar/NavBar.vue
@@ -0,0 +1,90 @@
+<script setup>
+import { ref, inject } from 'vue';
+import MegaMenu from './MegaMenuCategory.vue';
+
+const show = ref(false)
+const mainTag = inject('main')
+
+function hover() {
+    show.value = true
+    mainTag.value.classList.add("overlay")
+}
+
+function leave() {
+    show.value = false
+    mainTag.value.classList.remove("overlay")
+}
+
+function hover1() {
+    mainTag.value.classList.add("overlay")
+}
+
+function leave1() {
+    mainTag.value.classList.remove("overlay")
+}
+
+</script>
+
+<template>
+    <nav class="w-full flex items-center justify-between p-2 pr-4 ">
+        <div class="relative flex items-center">
+            <div class="relative flex flex-col text-sm text-neutral700 font-semibold " @mouseover="hover"
+                @mouseleave="leave">
+                <span class="pl-4 border-l border-neutral200 cursor-pointer">
+                    <i class="uil uil-apps text-lg"></i>
+                    دسته&zwnj;بندی کالاها
+                </span>
+                <div class="absolute w-full top-4">&nbsp;</div>
+            <div class="mega-menu-d absolute top-[28px] opacity- flex bg-neutral000  text-white z-50 shadow-md rounded-b-md transition-all duration-700"
+                :class="{'opacity-100 ' : show}"
+                    style="width: calc(100vw - 40%);" v-show="show" @mouseover="hover1" @mouseleave="leave1">
+                    <MegaMenu />
+                </div>
+            </div>
+            <ul class="flex items-center px-3 border-l border-neutral200">
+                <li class="ml-3">
+                    <a href="#" class="text-sm text-neutral700">
+                        <i class="uil uil-shopping-basket text-iconLowEmphasis text-lg"></i>
+                        سوپر&zwnj;مارکت
+                    </a>
+                </li>
+                <li class="ml-3">
+                    <a href="#" class="text-sm text-neutral700">
+                        <i class="uil uil-fire text-iconLowEmphasis text-lg"></i>
+                        پرفروش&zwnj;ترین&zwnj;ها
+                    </a>
+                </li>
+                <li class="ml-3">
+                    <a href="#" class="text-sm text-neutral700">
+                        <i class="uil uil-percentage  text-iconLowEmphasis text-lg"></i>
+                        تخفیف&zwnj;ها و پیشنهاد&zwnj;ها
+                    </a>
+                </li>
+                <li class="ml-2">
+                    <a href="#" class="text-sm text-neutral700">
+                        <i class="uil uil-pricetag-alt  text-iconLowEmphasis text-lg"></i>
+                        شگفت&zwnj;انگیزها
+                    </a>
+                </li>
+            </ul>
+            <ul class="flex items-center px-3">
+                <li class="ml-3">
+                    <a href="#" class="text-sm text-neutral700">سوالی دارید؟</a>
+                </li>
+                <li class="ml-3">
+                    <a href="#" class="text-sm  text-neutral700"> فروشنده شوید! </a>
+                </li>
+            </ul>
+            <div class="absolute bottom-0 right-0 w-[120px] h-[2px] bg-primary700"> </div>
+        </div>
+        <div>
+            <div class="flex items-center cursor-pointer" @clicl="showModalCity">
+                <i class="uil uil-map-marker text-iconHighEmphasis ml-2 text-xl"></i>
+                <p class="text-xs font-semibold text-neutral700">لطفا شهر خود را انتخاب کنید</p>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<style>
+</style>
