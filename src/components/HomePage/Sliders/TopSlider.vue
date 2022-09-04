e42323c5fd52123bf74a267cdc6e22e6d75a4d5d
Erfanprg1701 <erfan.prg1701@GMAIL.com>
2022-09-04 12:30:16 +0430

    add TopSlider component

---
+++ src/components/HomePage/Sliders/TopSlider.vue
@@ -0,0 +1,68 @@
+<script setup>
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import { Navigation, Pagination, Autoplay } from "swiper";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import 'swiper/css';
+import { defineProps } from 'vue';
+import { ref } from 'vue';
+
+const props = defineProps(["topSlider"])
+
+const modules = [Navigation, Pagination, Autoplay]
+
+</script>
+
+<template>
+    <swiper :slides-per-view="1" :rewind="true" :navigation="true" :pagination="{
+        clickable: true,
+    }" :autoplay="{
+    delay: 2500,
+    disableOnInteraction: false,
+}" :modules="modules"  class="slider h-full text-center cursor-pointer"
+        :style="{
+            '--swiper-navigation-color': '#424750',
+            '--swiper-navigation-size': '12px'
+        }">
+
+        <swiper-slide class="h-full" v-for="data in topSlider.slice(0, 8)" :key="data.id">
+            <a :href="data.src" class="h-full w-full" target="_blank">
+                <img :src="data.imgSlide" class="h-full object-cover" alt="slider">
+            </a>
+        </swiper-slide>
+    </swiper>
+
+</template>
+
+<style>
+.swiper-rtl .swiper-button-prev {
+    right: 35px;
+}
+
+.swiper-rtl .swiper-button-next {
+    right: 80px;
+}
+
+.swiper-horizontal>.swiper-pagination-bullets,
+.swiper-pagination-bullets.swiper-pagination-horizontal,
+.swiper-pagination-custom,
+.swiper-pagination-fraction {
+    bottom: 10px;
+    right: 0;
+    left: auto;
+    width: auto;
+    margin-right: 25px;
+}
+
+.swiper-pagination-bullet {
+    width: 6px;
+    height: 6px;
+}
+
+.swiper-pagination-bullet-active {
+    background-color: #e0e0e0;
+    width: 18px;
+    border-radius: 16px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,17 @@
 <script setup>
+import TopSlider from '@/components/HomePage/Sliders/TopSlider.vue';
+import topSlider from '@/data/topSlider';
+import { ref } from 'vue';
 
+const dataTopSlider = ref(topSlider)
 
 </script>
 
 <template>
-
+    <div id="content" class="w-full relative">
+        <div ref="main"></div>
+        <div class="w-full h-[400px] mb-4 text-center cursor-pointer">
+            <TopSlider :topSlider="dataTopSlider" />
+        </div>
+    </div>
 </template>
