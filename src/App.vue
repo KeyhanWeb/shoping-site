fcac4537d8fd6258e78155a83f4e78a33fba1c87
Erfanprg1701 <erfan.prg1701@GMAIL.com>
2022-09-04 11:21:09 +0430

    add style font

--- src/App.vue
+++ src/App.vue
@@ -6,4 +6,20 @@
   <router-view />
 </template>
 
-<style></style>
+<style>
+body {
+  font-family: 'yekan';
+  direction: rtl;
+
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  outline: none;
+  list-style: none;
+  text-decoration: none;
+  box-sizing: border-box;
+  font-family: 'yekan';
+}
+</style>
