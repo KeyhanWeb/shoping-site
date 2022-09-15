a4fc51f58b5ede4db6cae98576eab7bba51d1673
Erfanprg1701 <erfan.prg1701@GMAIL.com>
2022-09-15 15:08:50 +0430

    add slider offer component

--- src/views/SuperMarket.vue
+++ src/views/SuperMarket.vue
@@ -1,11 +1,16 @@
 <script setup>
-import TopSlider from '@/components/global/Sliders/TopSlider.vue';
-import NavMenu from '@/components/super-market/Nav-menu/NavMenu.vue';
-
-import topSlider from '@/data/super-market/slider-top';
 import { ref } from 'vue';
 
+import TopSlider from '@/components/global/Sliders/TopSlider.vue';
+import NavMenu from '@/components/super-market/Nav-menu/NavMenu.vue';
+import SliderOffer from '@/components/global/Sliders/SliderOffer.vue';
+
+import topSlider from '@/data/super-market/slider-top';
+import offerAmazing from '@/data/offerAmazing';
+
 const dataTopSlider = ref(topSlider)
+const dataOfferAmazing = ref(offerAmazing)
+
 
 </script>
 
@@ -14,9 +19,10 @@ const dataTopSlider = ref(topSlider)
         <div ref="main"></div>
         <div class="max-w-[1336px] lg:px-4 flex flex-col mx-auto">
             <NavMenu/>
-            <div class="w-full h-[400px] mb-4 my-2  text-center cursor-pointer">
+            <div class="w-full h-[400px] mb-2 my-2 text-center cursor-pointer">
                 <TopSlider :topSlider="dataTopSlider" class="rounded-2xl" />
             </div>
+            <SliderOffer :dataOffer="dataOfferAmazing"/>
         </div>
     </div>
 </template>
